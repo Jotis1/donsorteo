4afd85e498836be7f733fbdb979752c0021450a3
Jotis1 <cuellarjotis@gmail.com>
2023-10-17 12:35:48 +0200

    Real Data Update

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         {carrousel == 0 ? (
           <section className="z-[1] animate-fadeInOut flex-grow flex flex-col justify-center items-center gap-2.5">
             <p className="text-4xl font-black">Sorteo Tarjeta Amazon 100€</p>
-            <p className="text-pink-100">Inscríbete en aquí y gana una tarjeta regalo de Amazon valorada en 100€</p>
+            <p className="text-white/90">Inscríbete en aquí y gana una tarjeta regalo de Amazon valorada en 100€</p>
             <Link href={`/amazon_gift_card_100`} className="bg-pink-600 px-4 py-2 text-white w-fit mt-10 rounded-md hover:bg-white hover:text-pink-600 transition-all">Participa ahora</Link>
             <span className="absolute -z-[1] bg-neutral-600/60 w-full h-full"></span>
             <Image alt="fondo 1" className="-z-[2]" fill objectFit="cover" src={`/Fondo1.png`}></Image>
@@ -36,7 +36,7 @@ export default function Home() {
         ) : carrousel == 1 ? (
           <header className="z-[1] animate-fadeInOut flex-grow flex flex-col justify-center items-center gap-2.5">
             <p className="text-4xl font-black">Sorteo Samsung Galaxy M13</p>
-            <p className="text-pink-100">Inscríbete en aquí y gana un móvil Samsung Galaxy M13 valorado en +200€</p>
+            <p className="text-white/90">Inscríbete en aquí y gana un móvil Samsung Galaxy M13 valorado en +200€</p>
             <Link href={`/amazon_gift_card_100`} className="bg-pink-600 px-4 py-2 text-white w-fit mt-10 rounded-md hover:bg-white hover:text-pink-600 transition-all">Participa ahora</Link>
             <span className="absolute -z-[1] bg-neutral-600/60 w-full h-full"></span>
             <Image alt="fondo 2" className="-z-[2]" fill objectFit="cover" src={`/Fondo2.png`}></Image>
@@ -44,7 +44,7 @@ export default function Home() {
         ) : carrousel == 2 && (
           <div className="z-[1] animate-fadeInOut flex-grow flex flex-col justify-center items-center gap-2.5">
             <p className="text-4xl font-black">Sorteo Tablet Facetel</p>
-            <p className="text-pink-100">Inscríbete en aquí y gana una tablet Facetel valorada en +200€</p>
+            <p className="text-white/90">Inscríbete en aquí y gana una tablet Facetel valorada en +200€</p>
             <Link href={`/amazon_gift_card_100`} className="bg-pink-600 px-4 py-2 text-white w-fit mt-10 rounded-md hover:bg-white hover:text-pink-600 transition-all">Participa ahora</Link>
             <span className="absolute -z-[1] bg-neutral-600/60 w-full h-full"></span>
             <Image alt="fondo 3" className="-z-[2]" fill objectFit="cover" src={`/Fondo3.png`}></Image>
@@ -63,21 +63,21 @@ export default function Home() {
         </header>
         <section className="flex py-2 gap-5 justify-center flex-wrap">
           <Link href={`/giveaway/amazon_gift_card_100`} className="flex gap-2.5 flex-col min-w-[320px] text-orange-500 font-medium p-2.5 rounded-md bg-white hover:bg-orange-100 transition-all border-orange-300 border">
-            <section className="w-full h-40 bg-orange-400 rounded-md"></section>
+            <section style={{ backgroundImage: "url(/AmazonGiftCard.png)" }} className="w-full h-40 bg-white rounded-md bg-contain bg-no-repeat bg-center"></section>
             <header className="flex-grow text-center">
               <p className="font-bold">Tarjeta de Amazon de 100€</p>
             </header>
           </Link>
           <Link href={`#!`} className="flex gap-2.5 flex-col min-w-[320px] text-orange-500 font-medium p-2.5 rounded-md bg-white hover:bg-orange-100 transition-all border-orange-300 border">
-            <section className="w-full h-40 bg-orange-400 rounded-md"></section>
+            <section style={{ backgroundImage: "url(/GalaxyM13.jpg)" }} className="w-full h-40 bg-white rounded-md bg-contain bg-no-repeat bg-center"></section>
             <header className="flex-grow text-center">
-              <p className="font-bold">Móvil Samsung Galaxy</p>
+              <p className="font-bold">Móvil Samsung Galaxy M13</p>
             </header>
           </Link>
           <Link href={`#!`} className="flex gap-2.5 flex-col min-w-[320px] text-orange-500 font-medium p-2.5 rounded-md bg-white hover:bg-orange-100 transition-all border-orange-300 border">
-            <section className="w-full h-40 bg-orange-400 rounded-md"></section>
+            <section style={{ backgroundImage: "url(/TabletFacetel.jpg)" }} className="w-full h-40 bg-white rounded-md bg-contain bg-no-repeat bg-center"></section>
             <header className="flex-grow text-center">
-              <p className="font-bold">Tablet Samsung Galaxy</p>
+              <p className="font-bold">Tablet Facetel</p>
             </header>
           </Link>
         </section>
@@ -89,42 +89,42 @@ export default function Home() {
         </header>
         <section className="flex items-center gap-2.5 justify-center flex-wrap">
           <section className="flex flex-col items-center gap-2.5 p-5">
-            <figure className="w-32 h-32 bg-pink-500 rounded-full"></figure>
+            <figure style={{ backgroundImage: "url(/Avatar.webp)" }} className="w-32 h-32 bg-contain bg-no-repeat bg-center bg-pink-500 border-2 border-pink-500 rounded-full"></figure>
             <header className="text-center">
               <p className="text-xl text-pink-600 font-bold">Juan Manuel Cuellar</p>
               <p className="text-sm text-pink-600 font-medium">Ganador del Samsung Galaxy M13</p>
             </header>
           </section>
           <section className="flex flex-col items-center gap-2.5 p-5">
-            <figure className="w-32 h-32 bg-pink-500 rounded-full"></figure>
+            <figure style={{ backgroundImage: "url(/Avatar.webp)" }} className="w-32 h-32 bg-contain bg-no-repeat bg-center bg-pink-500 border-2 border-pink-500 rounded-full"></figure>
             <header className="text-center">
               <p className="text-xl text-pink-600 font-bold">Juan Manuel Cuellar</p>
               <p className="text-sm text-pink-600 font-medium">Ganador del Samsung Galaxy M13</p>
             </header>
           </section>
           <section className="flex flex-col items-center gap-2.5 p-5">
-            <figure className="w-32 h-32 bg-pink-500 rounded-full"></figure>
+            <figure style={{ backgroundImage: "url(/Avatar.webp)" }} className="w-32 h-32 bg-contain bg-no-repeat bg-center bg-pink-500 border-2 border-pink-500 rounded-full"></figure>
             <header className="text-center">
               <p className="text-xl text-pink-600 font-bold">Juan Manuel Cuellar</p>
               <p className="text-sm text-pink-600 font-medium">Ganador del Samsung Galaxy M13</p>
             </header>
           </section>
           <section className="flex flex-col items-center gap-2.5 p-5">
-            <figure className="w-32 h-32 bg-pink-500 rounded-full"></figure>
+            <figure style={{ backgroundImage: "url(/Avatar.webp)" }} className="w-32 h-32 bg-contain bg-no-repeat bg-center bg-pink-500 border-2 border-pink-500 rounded-full"></figure>
             <header className="text-center">
               <p className="text-xl text-pink-600 font-bold">Juan Manuel Cuellar</p>
               <p className="text-sm text-pink-600 font-medium">Ganador del Samsung Galaxy M13</p>
             </header>
           </section>
           <section className="flex flex-col items-center gap-2.5 p-5">
-            <figure className="w-32 h-32 bg-pink-500 rounded-full"></figure>
+            <figure style={{ backgroundImage: "url(/Avatar.webp)" }} className="w-32 h-32 bg-contain bg-no-repeat bg-center bg-pink-500 border-2 border-pink-500 rounded-full"></figure>
             <header className="text-center">
               <p className="text-xl text-pink-600 font-bold">Juan Manuel Cuellar</p>
               <p className="text-sm text-pink-600 font-medium">Ganador del Samsung Galaxy M13</p>
             </header>
           </section>
           <section className="flex flex-col items-center gap-2.5 p-5">
-            <figure className="w-32 h-32 bg-pink-500 rounded-full"></figure>
+            <figure style={{ backgroundImage: "url(/Avatar.webp)" }} className="w-32 h-32 bg-contain bg-no-repeat bg-center bg-pink-500 border-2 border-pink-500 rounded-full"></figure>
             <header className="text-center">
               <p className="text-xl text-pink-600 font-bold">Juan Manuel Cuellar</p>
               <p className="text-sm text-pink-600 font-medium">Ganador del Samsung Galaxy M13</p>
